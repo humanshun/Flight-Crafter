78eed45dbdfdb99e6860900d4196a04ae3798cf0
humanshun <human.kagoshima.shindomeshunsuke@gmail.com>
2025-02-26 16:57:31 +0900

    ショップデータ作成

--- Flight-Crafter/Assets/Data-------------/Parts/PartData.cs
+++ Flight-Crafter/Assets/Data-------------/Parts/PartData.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 public class PartData : ScriptableObject
 {
@@ -13,4 +14,12 @@ public class PartData : ScriptableObject
     [Header("金額")]
     [Tooltip("このパーツの価格")]
     public int partCost;
+
+    [Header("アイコン")]
+    [Tooltip("このパーツのアイコン")]
+    public Sprite partIcon; // パーツのアイコン
+
+    [Header("タイプ")]
+    [Tooltip("このパーツの種類")]
+    public PartType partType; // パーツの種類
 }
--- Flight-Crafter/Assets/Scripts-----------/Shop/Shop.cs
+++ Flight-Crafter/Assets/Scripts-----------/Shop/Shop.cs
@@ -5,35 +5,35 @@ using TMPro;
 public class Shop : MonoBehaviour
 {
     public PlayerData playerData; // プレイヤーデータの参照
-    public PartData[] shopItems; // ショップアイテムのデータ配列
-    public Button[] buyButtons; // 購入ボタンの配列
-    public TextMeshProUGUI[] itemNameText; // アイテム表示用のテキスト
+    public ShopData shopData; // ショップアイテムのデータ配列
+    // public Button[] buyButtons; // 購入ボタンの配列
+    // public TextMeshProUGUI[] itemNameText; // アイテム表示用のテキスト
 
-    void Start()
-    {
-        for (int i = 0; i < shopItems.Length; i++)
-        {
-            if (i < buyButtons.Length && buyButtons[i] != null)
-            {
-                int index = i; // ローカル変数にインデックスを保存
-                buyButtons[i].onClick.AddListener(() => BuyItem(shopItems[index], buyButtons[index]));
-                buyButtons[i].GetComponentInChildren<TextMeshProUGUI>().text = shopItems[index].partCost + "$";
-                itemNameText[i].text = shopItems[index].partName;
-            }
-        }
-    }
+    // void Start()
+    // {
+    //     for (int i = 0; i < shopItems.Length; i++)
+    //     {
+    //         if (i < buyButtons.Length && buyButtons[i] != null)
+    //         {
+    //             int index = i; // ローカル変数にインデックスを保存
+    //             buyButtons[i].onClick.AddListener(() => BuyItem(shopItems[index], buyButtons[index]));
+    //             buyButtons[i].GetComponentInChildren<TextMeshProUGUI>().text = shopItems[index].partCost + "$";
+    //             itemNameText[i].text = shopItems[index].partName;
+    //         }
+    //     }
+    // }
 
-    void BuyItem(PartData item, Button button)
-    {
-        if (playerData.playerCoins >= item.partCost)
-        {
-            playerData.playerCoins -= item.partCost;
-            Debug.Log(playerData.playerCoins);
-            button.interactable = false; // ボタンを無効にする
-        }
-        else
-        {
-            Debug.Log("コインが不足しています。");
-        }
-    }
+    // void BuyItem(PartData item, Button button)
+    // {
+    //     if (playerData.playerCoins >= item.partCost)
+    //     {
+    //         playerData.playerCoins -= item.partCost;
+    //         Debug.Log(playerData.playerCoins);
+    //         button.interactable = false; // ボタンを無効にする
+    //     }
+    //     else
+    //     {
+    //         Debug.Log("コインが不足しています。");
+    //     }
+    // }
 }
