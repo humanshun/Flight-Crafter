5e5eb932e7f5b2044410f0f1649bdaaed0c401be
humanshun <human.kagoshima.shindomeshunsuke@gmail.com>
2025-05-25 03:04:22 +0900

    [add] バーを100をマックスで割合で表示

--- Flight-Crafter/Assets/Scripts-----------/Shop/StatusBar.cs
+++ Flight-Crafter/Assets/Scripts-----------/Shop/StatusBar.cs
@@ -6,9 +6,12 @@ public class StatusBar : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI statusNameText; // ステータス名のテキスト
     [SerializeField] private TextMeshProUGUI statusValueText; // ステータス値のテキスト
+    [SerializeField] private RectTransform statusValue;
     public void Setup(string displayName, float value)
     {
         statusNameText.text = displayName; // ステータス名を設定
         statusValueText.text = value.ToString(); // ステータス値を設定
+
+        statusValue.anchorMax = new Vector2(value / 100, statusValue.anchorMax.y); // ステータス値に応じてUIを調整
     }
 }
