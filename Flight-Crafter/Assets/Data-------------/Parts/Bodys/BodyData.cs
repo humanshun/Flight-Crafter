ef12585efa8f3115bbb6f3acf4d8f0c7c3397fb9
humanshun <human.kagoshima.shindomeshunsuke@gmail.com>
2025-05-25 02:51:10 +0900

    [add] 体力ステータス追加

--- Flight-Crafter/Assets/Data-------------/Parts/Bodys/BodyData.cs
+++ Flight-Crafter/Assets/Data-------------/Parts/Bodys/BodyData.cs
@@ -9,6 +9,10 @@ public class BodyData : PartData
     [Tooltip("値が高いほど空気抵抗が大きくなります。")]
     public AirResistance airResistance;  // 抗力のプロパティ
 
+    [Header("体力")]
+    [Tooltip("値が高いほど体力が大きくなります。")]
+    public Health hp;  // 体力
+
     [Header("ロケットの位置")]
     [Tooltip("ロケットの位置を指定します。")]
     public Vector2 rocketPosition;  // ロケットの位置
@@ -26,6 +30,13 @@ public class BodyData : PartData
     public class AirResistance
     {
         [HideInInspector] public string displayName = "空気抵抗";
+        public float value;
+    }
+
+    [System.Serializable]
+    public class Health
+    {
+        [HideInInspector] public string displayName = "体力";
         public float value;
     }
 }
