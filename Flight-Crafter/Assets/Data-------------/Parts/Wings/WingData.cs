6a8fc78cfe48a1d1e3b3f9b6b925aea5af86ee28
humanshun <human.kagoshima.shindomeshunsuke@gmail.com>
2025-05-25 02:53:06 +0900

    [update] 揚力、回転制御、推進力を削除、空気抵抗追加

--- Flight-Crafter/Assets/Data-------------/Parts/Wings/WingData.cs
+++ Flight-Crafter/Assets/Data-------------/Parts/Wings/WingData.cs
@@ -3,44 +3,25 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Wing", menuName = "Parts/Wing")]
 public class WingData : PartData
 {
-    [Header("揚力")]
-    [Tooltip("値が高いほど水平方向に向いているときの、垂直方向の浮力が大きくなります。")]
-    public Lift lift;       // 揚力
-    
+    [Header("空気抵抗")]
+    [Tooltip("値が高いほど空気抵抗が大きくなります。")]
+    public AirResistance airResistance; //抗力のプロパティ
+
     [Header("空中制御")]
     [Tooltip("値が高いほど、空中での操作性が良くなります。")]
     public AirControl airControl; //空中制御
 
-    [Header("回転制御")]
-    [Tooltip("値が高いほど空中での回転の上限が高くなります。")]
-    public AirRotationalControl airRotationalControl;
-
-    [Header("推進力%")]
-    [Tooltip("値が高いほど、ベクトル推進力が強くなります。")]
-    public PropulsionPower propulsionPower; // 推進力の大きさ
-
     [System.Serializable]
-    public class Lift
+    public class AirResistance
     {
-        [HideInInspector] public string displayName = "揚力";
+        [HideInInspector] public string displayName = "空気抵抗";
         public float value;
     }
+    
     [System.Serializable]
     public class AirControl
     {
         [HideInInspector] public string displayName = "空中制御";
-        public float value;
-    }
-    [System.Serializable]
-    public class AirRotationalControl
-    {
-        [HideInInspector] public string displayName = "回転制御";
-        public float value;
-    }
-    [System.Serializable]
-    public class PropulsionPower
-    {
-        [HideInInspector] public string displayName = "推進力";
         public float value;
     }
 }
