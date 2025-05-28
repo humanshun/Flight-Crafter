2754dbd5e6f7e76ac90e2608d8c4af29a3dd9eb7
humanshun <human.kagoshima.shindomeshunsuke@gmail.com>
2025-05-28 11:05:40 +0900

    [update] 最高距離、最高高度の変数追加

--- Flight-Crafter/Assets/Scripts-----------/Data/PlayerSaveData.cs
+++ Flight-Crafter/Assets/Scripts-----------/Data/PlayerSaveData.cs
@@ -7,6 +7,9 @@ public class PlayerSaveData
     public int coins;
     public List<string> purchasedPartNames = new List<string>();
     public List<PartTypePartPair> currentParts;
+
+    public float maxDistance;
+    public float maxAltitude;
 }
 
 [System.Serializable]
